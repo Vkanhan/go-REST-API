a5ee2c394ca53c7dce69d9f0469b4a4df214126a
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-06-15 22:38:45 +0530

    add HTTP server for serving static files

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Go-REST-API")
+
+	// Define the root directory from which files will be served.
+	const filepathRoot = "."
+
+	// Define the port on which the server will listen.
+	const port = "8080"
+
+	// Create a new ServeMux, which is a HTTP request multiplexer.
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	// Create an HTTP server configuration.
+	server := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+
+	// Start the HTTP server to listen on the specified port.
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal("error listening to port 8080")
+	}
+
 }
+
+
