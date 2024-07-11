8046e2d92629fa646a4fb59289865baa0aaca233
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-07-11 13:29:54 +0530

    fix: correct handler registration and refactoring

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
-//apiConfig holds the counter for file server hits
+// apiConfig holds the counter for file server hits
 type apiConfig struct {
 	fileserverHits int
 }
@@ -20,7 +19,7 @@ func main() {
 	const port = "8080"
 
 	//Initialize the apiConfig struct with fileserverHits counter set to 0.
-	apiCfg := apiConfig {
+	apiCfg := apiConfig{
 		fileserverHits: 0,
 	}
 
@@ -32,13 +31,13 @@ func main() {
 	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 
 	// Register the health check endpoint.
-	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("GET /healthz", handlerReadiness)
 
 	//Register the metrics endpoint to show the file server hit count.
-	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
 
 	// Register the reset endpoint to reset the file server hit count.
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /reset", apiCfg.handlerReset)
 
 	// Create an HTTP server configuration.
 	server := http.Server{
@@ -53,19 +52,4 @@ func main() {
 		log.Fatal("error listening to port 8080")
 	}
 
-}
-
-// middlewareMetricsInc is a middleware that increments the file server hit counter for each request.
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits++
-		next.ServeHTTP(w, r) // Call the next handler in the chain.
-	})
-}
-
-// handleMetrics handles the metrics endpoint and returns how many time the fileserver got hit.
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
 }
